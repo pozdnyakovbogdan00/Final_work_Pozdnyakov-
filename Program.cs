ed9aa1a177ce256e2b57f11315b4c8015e6bcfcf
Bogdan <pozdnyakov.bogdan00@gmail.com>
2022-09-24 17:30:00 +0600

    Creare programm

--- Program.cs
+++ Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] GetUserData()
+{
+    Console.WriteLine("How many elements do you want to enter?");
+    int N = Convert.ToInt32(Console.ReadLine());
+    int i = 0;
+    string[] Array = new string[N];
+    while (i < N)
+    {
+        Console.WriteLine("Enter" + (i + 1) + " element");
+        Array[i] = Console.ReadLine();
+        i++;
+    }
+    return Array;
+}
+
+string[] GetAnArrayByCondition(string[] Array)
+{
+    int i = 0;
+    string[] OutPutArray = new string[Array.Length];
+    foreach (var stringArray in Array)
+    {
+        if (stringArray.Length <= 3)
+        {
+            OutPutArray[i] = stringArray;
+            i++;
+        }
+    }
+    return OutPutArray;
+
+}
+
+void OutputTheResult(string[] OutPutArray)
+{
+    string StringArray = "[ ";
+    foreach (var item in OutPutArray)
+    {
+        if (item != null)
+        {
+            StringArray = StringArray + item + "; ";    
+        } 
+    }
+    StringArray = StringArray + "]";
+
+    Console.WriteLine("Result: ");
+    Console.WriteLine(StringArray);
+}
+
+
+string[] Array = GetUserData();
+string[] OutPutArray = GetAnArrayByCondition(Array);
+OutputTheResult(OutPutArray);
+
+
